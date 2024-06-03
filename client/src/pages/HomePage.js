641a0c9c42fb61374fedfc14c6d98981342d198c
sushill_sharma <sushilssharma064@gmail.com>
2024-06-03 06:39:43 +0530

    error resolve

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,7 @@ import { useCart } from "../context/cart";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Layout from "./../components/Layout/Layout";
-import { AiOutlineReload } from "react-icons/ai";
+// import { AiOutlineReload } from "react-icons/ai";
 import "../styles/Homepage.css";
 
 
@@ -209,7 +209,7 @@ const HomePage = () => {
                 ) : (
                   <>
                     {" "}
-                    Loadmore <AiOutlineReload />
+                    Loadmore 
                   </>
                 )}
               </button>
